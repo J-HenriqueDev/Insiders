2271c0fd4450f4d989f567d940b1c4534b1be041
Neo_ <45069999+neooprogrammer@users.noreply.github.com>
2020-11-19 16:29:50 -0300

    Update role.py

--- cogs/utils/role.py
+++ cogs/utils/role.py
@@ -1,10 +1,9 @@
 import json
 
-with open("utils/cargos-membros.json", encoding="utf-8") as cargos:
+with open("cogs/utils/cargos-membros.json", encoding="utf-8") as cargos:
     cargos = json.load(cargos)
 
-with open("reactionrole.json", encoding="utf-8") as react:
-    reactions = json.load(react)
 
-with open("utils/emojis.json", encoding="utf-8") as emj:
+
+with open("cogs/utils/emojis.json", encoding="utf-8") as emj:
     emojis = json.load(emj)
